2ea129036e3ee109212ab5a6973f9eba915a2bba
alisdairdaniel <117979215+alisdairdaniel@users.noreply.github.com>
2023-01-13 01:17:23 -0800

    Update py_os.py

--- py_os.py
+++ py_os.py
@@ -1,18 +1,13 @@
 import os
 
 # Target directory
-targetDir = r'd:\tmp\util\dist\check'
+
+targetDir = "/demo/participant"
 files = []
 dirs  = []
-
-# The three variables below, dirpath.dirnamesfilenames
-# dirpath represents the directory name that is currently traversal
-# dirnames is a list object that stores all the subclass names in the current dirpath
-# filenames is a list object that stores all the file names in the current dirpath
 
 for (dirpath, dirnames, filenames) in os.walk(targetDir):
     files += filenames
     dirs += dirnames
-
 print(files)
 print(dirs)
